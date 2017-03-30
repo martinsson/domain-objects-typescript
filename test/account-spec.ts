c7d20eca818524914dc455ce4f6bbcea1af968b4
Johan MARTINSSON <martinsson.johan@gmail.com>
2017-03-30 23:12:32 +0200

    write out the acceptance test

--- test/account-spec.ts
+++ test/account-spec.ts
@@ -3,9 +3,34 @@ import * as chai from "chai";
 
 const expect = chai.expect;
 
+class Account {
+
+  public printStatement() {
+
+  }
+
+  public deposit(amount: number) {
+
+  }
+
+  public withdraw(amount: number) {
+
+  }
+}
+
 describe("account", () => {
-  it("does something", () => {
-    expect(true).to.equal(false);
+  it.skip("does something", () => {
+    const account = new Account();
+    account.deposit(500);
+    account.withdraw(100);
+    const statementString = account.printStatement();
+    expect(statementString).to.equal(`
+Date        Amount  Balance
+24.12.2015   +500      500
+23.8.2016    -100      400
+    `);
   });
+
+
 });
 
